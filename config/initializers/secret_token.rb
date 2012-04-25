ebe1de4498ce933c1ff1a849d852785e8df168a1
Sean Powers <spsimplicity@gmail.com>
2012-04-24 19:29:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CPG::Application.config.secret_token = '49870a392c15304fda380670151dd394a62a2c62d4d71c5bcdac69c2bf7a3258ecf83c35c42d6e4f9668a81d1ad4d3784d64c13789e1ba11de56d0c12ffe527a'
